6eeb9492dcdf0d5f483e66f2fc2b228559b821ec
warrenbrown <warren@katapultmedia.com>
2017-12-02 10:24:55 -0600

    Added filter to categories

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,13 @@ class MoviesController < ApplicationController
   before_action :set_movie, only:[ :show, :edit, :update, :destroy ]
 
   def index
-    @movies = Movie.all
+    if params[:category].blank?
+      @movies = Movie.all.order("created_at DESC")
+    else
+      #@category_id = Category.find(name: params[:category]).id
+      @category_id = Category.find_by(name: params[:category]).id
+      @movies = Movie.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 
   def new
